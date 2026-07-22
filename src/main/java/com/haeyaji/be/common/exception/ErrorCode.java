4766152b1f77dc57dbca182963933df8cf8c4734
김현우 <rlagusdn0914@naver.com>
2026-07-22 20:32:56 +0900

    feat: USER_NOT_FOUND 에러코드 추가

--- src/main/java/com/haeyaji/be/common/exception/ErrorCode.java
+++ src/main/java/com/haeyaji/be/common/exception/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode implements ResponseCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다")
+
     ;
     private final HttpStatus status;
     private final String message;

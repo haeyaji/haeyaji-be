81012c4e7d169a050e626bc4088fe1af532b166f
김현우 <rlagusdn0914@naver.com>
2026-07-23 09:27:53 +0900

    feat: name컬럼 삭제, nickname/friendcode 컬럼 추가 (#18)

--- src/main/java/com/haeyaji/be/user/domain/User.java
+++ src/main/java/com/haeyaji/be/user/domain/User.java
@@ -25,7 +25,10 @@ public class User extends MutableBaseEntity {
     @Column(name = "social_type_id", nullable = false)
     private String socialTypeId;   // 소셜 타입 안에서의 고유 ID
 
-    private String name;
+    private String nickname;
+
+    @Column(name = "friend_code", nullable = false, length = 10, unique = true)
+    private String friendCode;
 
     private String email;
 
@@ -34,16 +37,16 @@ public class User extends MutableBaseEntity {
     private UserRole role;
 
     @Builder
-    private User(SocialType socialType, String socialTypeId, String name, String email, UserRole role) {
+    private User(SocialType socialType, String socialTypeId, String nickname, String friendCode, String email, UserRole role) {
         this.socialType = socialType;
         this.socialTypeId = socialTypeId;
-        this.name = name;
+        this.nickname = nickname;
+        this.friendCode = friendCode;
         this.email = email;
         this.role = (role != null) ? role : UserRole.ROLE_USER;
     }
 
-    public User update(String name, String email) {
-        this.name = name;
+    public User update(String email) {
         this.email = email;
 
         return this;

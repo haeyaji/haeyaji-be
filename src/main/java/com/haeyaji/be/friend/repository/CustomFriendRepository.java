bf4c381f74265eb192c8cc9d7d0f1fc703629606
김현우 <rlagusdn0914@naver.com>
2026-07-23 18:32:25 +0900

    feat: 불필요한 import 삭제 (#35)

--- src/main/java/com/haeyaji/be/friend/repository/CustomFriendRepository.java
+++ src/main/java/com/haeyaji/be/friend/repository/CustomFriendRepository.java
@@ -1,8 +1,6 @@
 package com.haeyaji.be.friend.repository;
 
 import com.haeyaji.be.friend.domain.Friend;
-import com.haeyaji.be.friend.domain.FriendStatus;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
--- src/main/java/com/haeyaji/be/friend/repository/FriendRepository.java
+++ src/main/java/com/haeyaji/be/friend/repository/FriendRepository.java
@@ -3,8 +3,6 @@ package com.haeyaji.be.friend.repository;
 import com.haeyaji.be.friend.domain.Friend;
 import com.haeyaji.be.friend.domain.FriendStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
